4491b173af9b586a4144f765c911d787fb810c2b
langdk <107159417+DanLang1@users.noreply.github.com>
2025-03-04 21:11:22 -0600

    adding suspense to login page

---
+++ app/login/loading.tsx
@@ -0,0 +1,5 @@
+import { Loader } from '@mantine/core';
+
+export default function loading() {
+  return <Loader color="blue" />;
+}
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,11 @@
+import { Suspense } from 'react';
 import { AuthenticationForm } from '@/components/AuthenticationForm';
 import { login, signup } from './actions';
 
 export default function LoginPage() {
-  return <AuthenticationForm />;
+  return (
+    <Suspense>
+      <AuthenticationForm />
+    </Suspense>
+  );
 }
